6aa676e0208ed339871a685d62c482cedeae7b75
leemac33 <lee.mccormick@borngroup.com>
2021-02-11 13:34:41 -0500

    added app and document file for page layout

---
+++ frontend/pages/_app.js
@@ -0,0 +1,12 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable react/prop-types */
+import Page from '../components/Page';
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <Page>
+      <Component {...pageProps} />
+    </Page>
+  );
+}
